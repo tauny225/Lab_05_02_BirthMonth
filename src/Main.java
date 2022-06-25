a5c55981a96e95c1357257c054219c847e5a44c0
DJEDOU KODJO <tauny225@gmail.com>
2022-06-25 04:38:44 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int birthMonth = 3;
+
+        if (birthMonth > 12)
+            {
+                System.out.println("You entered an incorrect month value: " + birthMonth);
+            }
+        else
+            {
+                System.out.println("Your birth month is: " + birthMonth);
+            }
+
+        // write your code here
+    }
+}
